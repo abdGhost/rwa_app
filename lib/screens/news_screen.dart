7cea68267bf133d70744620528a6acb187fc3b71
abdGhost <ktaher642@gmail.com>
2025-04-19 14:45:57 +0530

    added blogs screen

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rwa_app/widgets/search_appbar_field_widget.dart';
+import 'package:rwa_app/widgets/blogs/blogs_card.dart';
 import 'package:rwa_app/widgets/news/news_appbar_title_row.dart';
 import 'package:rwa_app/widgets/news/news_tab_buttons.dart';
 import 'package:rwa_app/widgets/news/news_card_main.dart';
 import 'package:rwa_app/widgets/news/news_card_side.dart';
+import 'package:rwa_app/widgets/search_appbar_field_widget.dart';
 
 class NewsScreen extends StatefulWidget {
   const NewsScreen({super.key});
@@ -69,17 +70,15 @@ class _NewsScreenState extends State<NewsScreen> {
     final newsItems = [
       {
         'image': 'assets/news_blogs/news_1.png',
-        'title':
-            'RWA Protocols Cross \$10B in Total Value Locked - Rise over regulatory risks, liquidity, and inves',
-        'subtitle':
-            'Concerns rise over regulatory risks, liquidity, and investor protection. Rise over regulatory risks, liquidity, and inves,Rise over regulatory risks, liquidity, and inves',
+        'title': 'RWA Protocols Cross \$10B in Total Value Locked...',
+        'subtitle': 'Concerns rise over regulatory risks...',
         'source': 'CryptoSlate',
         'time': '21 minutes ago',
       },
       {
         'image': 'assets/news_blogs/news_2.png',
-        'title': 'China’s CPIC Launches \$100M Tokenized RWA Fund with HashKey',
-        'subtitle': 'CPIC and HashKey bring institutional capital to RWA.',
+        'title': 'China’s CPIC Launches \$100M Tokenized RWA Fund...',
+        'subtitle': 'CPIC and HashKey bring institutional capital...',
         'source': 'CoinDesk',
         'time': '42 minutes ago',
       },
@@ -89,6 +88,20 @@ class _NewsScreenState extends State<NewsScreen> {
         'subtitle': '',
         'source': 'Blockchain.News',
         'time': '1 hour ago',
+      },
+      {
+        'image': 'assets/news_blogs/news_1.png',
+        'title': 'Top 5 RWA Projects You Can’t Miss in 2024',
+        'subtitle': '',
+        'source': 'Blockchain.News',
+        'time': '2 hours ago',
+      },
+      {
+        'image': 'assets/news_blogs/news_1.png',
+        'title': 'Top 5 RWA Projects You Can’t Miss in 2024',
+        'subtitle': '',
+        'source': 'Blockchain.News',
+        'time': '2 hours ago',
       },
       {
         'image': 'assets/news_blogs/news_1.png',
@@ -113,60 +126,29 @@ class _NewsScreenState extends State<NewsScreen> {
     final blogs = [
       {
         'title': 'The Hidden Risks in RWA Protocols You Need to Know',
+        'subtitle': 'RWAs promise stability and yield...',
         'author': 'Alex Foster',
         'time': '7 Minutes read',
+        'image': 'assets/news_blogs/news_1.png',
       },
       {
         'title': 'RWA Airdrops: The Next Big Opportunity in DeFi?',
+        'subtitle': 'Token airdrops are becoming a powerful way...',
         'author': 'Vanessa Liu',
         'time': '4 Minutes read',
       },
       {
         'title': 'Real Estate on the Blockchain: A Quiet Revolution',
+        'subtitle': 'Lowering investment barriers...',
         'author': 'Priya Desai',
         'time': '6 Minutes read',
+        'image': 'assets/news_blogs/news_1.png',
       },
     ];
 
     return ListView(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      children:
-          blogs
-              .map(
-                (blog) => _blogCard(
-                  title: blog['title']!,
-                  author: blog['author']!,
-                  time: blog['time']!,
-                ),
-              )
-              .toList(),
-    );
-  }
-
-  Widget _blogCard({
-    required String title,
-    required String author,
-    required String time,
-  }) {
-    return Card(
-      margin: const EdgeInsets.only(bottom: 12),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
-            ),
-            const SizedBox(height: 6),
-            Text(
-              '$author · $time',
-              style: const TextStyle(color: Colors.grey, fontSize: 12),
-            ),
-          ],
-        ),
-      ),
+      children: blogs.map((blog) => BlogCard(blog: blog)).toList(),
     );
   }
 }
